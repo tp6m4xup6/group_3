440dc2b974f3c6efae19cfbb6b6b6a2688373b70
oeg8168 <oeg8168@gmail.com>
2013-11-27 00:07:09 +0800

    完成輸出CSV資料
    public static boolean printCSV(ArrayList<ArrayList<String>> dataCSV)

--- Large and Open Source Project/src/LnOS.java
+++ Large and Open Source Project/src/LnOS.java
@@ -5,39 +5,38 @@ import java.util.ArrayList;
 
 public class LnOS {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
+
+		ArrayList<ArrayList<String>> CSVdata = null;
 
 		try {
-			readCSV("C:\\Users\\OEG\\Desktop\\test.CSV");
+			CSVdata = readCSV("C:\\Users\\OEG\\Desktop\\test.CSV");
+			
 		} catch (IOException e) {
-
 			System.err.println("Read .csv file error!");
 			e.printStackTrace();
 		}
-		
+		//printCSV(CSVdata);
 	}
 
 	/**
-	 * @author OEG
+	 * Read data from input .csv file.
 	 * 
+	 * @author OEG
 	 * @param path
 	 *            - Path of input .csv file
-	 * 
 	 * @return ArrayList that contain data in .csv file
-	 * 
 	 * @throws IOException
 	 *             - If an I/O error occurs
-	 * 
-	 * 
 	 */
-	@SuppressWarnings("rawtypes")
-	public static ArrayList<ArrayList> readCSV(String path) throws IOException {
+	public static ArrayList<ArrayList<String>> readCSV(String path)
+			throws IOException {
 
 		// Store all data in csv file
-		ArrayList<ArrayList> allData = new ArrayList<ArrayList>();
+		ArrayList<ArrayList<String>> allData = new ArrayList<ArrayList<String>>();
 
 		BufferedReader rd = new BufferedReader(new FileReader(path));
-		
+
 		// String to store data of single line temporarily
 		String line = null;
 
@@ -45,20 +44,46 @@ public class LnOS {
 
 			// Store data of single line in csv file
 			ArrayList<String> lineData = new ArrayList<String>();
-			
+
 			String item[] = line.split(",");
-			
+
 			// Add each item to lineData
-			for(int i=0; i<item.length; i++){            
-                lineData.add(i, item[i]);
-            }
-			
+			for (int i = 0; i < item.length; i++) {
+				lineData.add(i, item[i]);
+			}
+
 			allData.add(lineData);
 		}
 
 		rd.close();
 
 		return allData;
-	}	//end of readCSV()
+	} // end of readCSV()
 
-}	//end of LnOS
+	/**
+	 * Print all data in .csv file .
+	 * 
+	 * @author OEG
+	 * @param dataCSV
+	 *            - ArrayList that contain data in .csv file
+	 * @return true - print data successful <br/>
+	 *         false - print data fail
+	 */
+	public static boolean printCSV(ArrayList<ArrayList<String>> dataCSV) {
+		
+		if (dataCSV==null || dataCSV.isEmpty()) {
+			return false;
+		}
+
+		for (int i = 0; i < dataCSV.size(); i++) {
+			ArrayList<String> lineData = dataCSV.get(i);
+
+			for (int j = 0; j < lineData.size(); j++) {
+				System.out.println(lineData.get(j));
+			}
+		}
+
+		return true;
+	}
+
+} // end of LnOS
