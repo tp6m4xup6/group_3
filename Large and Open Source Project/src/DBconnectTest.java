ce8b4c29c1e25b6c31f56d7f3fe2eda9cc6aaf4b
oeg8168 <oeg8168@gmail.com>
2014-01-08 23:45:32 +0800

    new pattern

---
+++ Large and Open Source Project/src/DBconnectTest.java
@@ -0,0 +1,47 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+
+public class DBconnectTest {
+
+	@Test
+	public void testHiv_upload() {
+		
+//		boolean hiv_sucess= DBconnect.Hiv_upload("hiv.csv");
+//		assertTrue(hiv_sucess);
+		
+	}
+
+	@Test
+	public void testGetHivSQL() {
+		boolean getHiv=false;
+		ArrayList<ArrayList<String>>  dataHiv=new ArrayList<ArrayList<String>>();
+		dataHiv=DBconnect.getHivSQL();
+		if(dataHiv.size()>0){
+			getHiv=true;
+		}
+		assertTrue(getHiv);
+	}
+
+	@Test
+	public void testBorn_upload() {
+//		boolean born_sucess= DBconnect.Born_upload("born.csv");
+//		assertTrue(born_sucess);
+	}
+
+	@Test
+	public void testGetBornSQL() {
+		boolean getBorn=false;
+		ArrayList<ArrayList<String>>  dataBorn=new ArrayList<ArrayList<String>>();
+		dataBorn=DBconnect.getBornSQL();
+		if(dataBorn.size()>0){
+			getBorn=true;
+		}
+		assertTrue(getBorn);				
+		
+	}
+
+}
---
+++ Large and Open Source Project/src/PatternDataUpload.java
@@ -0,0 +1,20 @@
+
+
+
+public class PatternDataUpload {
+
+	/**
+	 * @author tp6m4xup6
+	 * @param DBconnect    
+	 * @return no return 
+	 */
+	public boolean DataUpload()  {
+
+		DBconnect.Hiv_upload("hiv.csv");
+		DBconnect.Born_upload("born.csv");
+
+		return true;
+
+	} // end of getCSVData()
+
+} // end of class PatternReadCSV
